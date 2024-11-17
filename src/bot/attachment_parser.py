3384b94c07cf17b0612906f2268a290dee15bd82
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2024-11-18 00:06:59 +0100

    Simplify attachment_parser.py

--- src/bot/attachment_parser.py
+++ src/bot/attachment_parser.py
@@ -4,6 +4,4 @@ from hikari import Attachment
 
 
 def parse_image_urls(attachments: Sequence[Attachment]) -> list[str]:
-    return [
-        attachment.url for attachment in attachments if "image" in attachment.media_type.lower()
-    ]
+    return [attachment.url for attachment in attachments]
